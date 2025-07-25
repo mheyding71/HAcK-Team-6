5d0a0679c4dd6515e800a1706ca2f0e0a36e86da
Andrew Nguyen <andrewnguyen@Andrews-MacBook-Air-3.local>
2025-07-25 14:52:08 -0700

    added api call feature --> need to connect front/back

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,6 +2,13 @@ import React, { useState, useEffect } from "react";
 import io from 'socket.io-client';
 import './App.css';
 
+
+
+
+// Connect to the Socket.IO server
+
+
+
 const socket = io('http://localhost:8000');
 
 function App() {
